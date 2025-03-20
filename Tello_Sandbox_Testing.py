171d5a07520db5cf7118b8713a2fee60472b653d
Konnerhunt81 <konnerhunt81@gmail.com>
2025-03-20 14:10:35 -0400

    Full Square

--- Tello_Sandbox_Testing.py
+++ Tello_Sandbox_Testing.py
@@ -44,9 +44,9 @@ recvThread.start()
 # CREATE FUNCTIONS HERE....
 
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\nKonner Hunt & Kent Chheu")
+print("Program Name:Square ")
+print("Date:3-20-25 ")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
@@ -60,7 +60,19 @@ try:
 
         sendmsg('takeoff', 8)
 
-        sendmsg('forward 100', 10)
+        sendmsg('forward 100', )
+
+        sendmsg('cw 90', 8)
+
+        sendmsg('forward 100', )
+
+        sendmsg('cw 90',8 )
+
+        sendmsg('forward 100', )
+
+        sendmsg('cw 90', 8)
+
+        sendmsg('forward 100', )
 
         sendmsg('cw 90', 8)
 
